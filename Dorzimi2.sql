3ef277c1229e2f467457e6bad8b9e5ed39c4edf1
Rijad Halili <rijadhalili.yt@gmail.com>
2023-12-30 17:47:34 +0100

    update

--- Dorzimi2.sql
+++ Dorzimi2.sql
@@ -552,3 +552,22 @@ WHERE ID_Koha IN (4,5);
 
 Select *
 From Koha
+
+UPDATE Ulesja
+SET Lloji = 'Family Seat', Klasa = 'Family', Vendi = '2Z'
+WHERE ID_Ulesja = 5;
+
+UPDATE Ulesja
+SET Lloji = 'VIP Seat', Klasa = 'VIP', Vendi = '3V'
+WHERE ID_Ulesja = 6;
+
+UPDATE Ulesja
+SET Lloji = 'Family Seat', Klasa = 'Family', Vendi = '4F'
+WHERE ID_Ulesja = 8;
+
+UPDATE Ulesja
+SET Lloji = 'VIP Seat', Klasa = 'VIP', Vendi = '2V'
+WHERE ID_Ulesja = 10;
+
+Select *
+From Ulesja
